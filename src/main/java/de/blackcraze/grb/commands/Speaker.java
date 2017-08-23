1e3d0d04e564ed37b13c47fa38c3f3422768d652
Sahil Youngs <syoungs@cisco.com>
2017-08-23 22:37:58 +0100

    Remove old multi-shoot say

--- src/main/java/de/blackcraze/grb/commands/Speaker.java
+++ src/main/java/de/blackcraze/grb/commands/Speaker.java
@@ -1,28 +1,12 @@
 package de.blackcraze.grb.commands;
 
-import de.blackcraze.grb.core.BotConfig;
 import net.dv8tion.jda.core.entities.TextChannel;
-
-import java.util.List;
-import java.util.Optional;
 
 public class Speaker {
 
 	public static class Reaction {
 		public final static String SUCCESS = "✔";
 		public final static String FAILURE = "✖";
-	}
-
-	public static void say(List<TextChannel> channels, String text) {
-		Optional<TextChannel> optionalChannel = channels.stream()
-				.filter(channel -> BotConfig.CHANNEL.equalsIgnoreCase(channel.getName()))
-				.findFirst();
-		if (!optionalChannel.isPresent()) {
-			System.err.printf("No channel found matching name:%s%n", BotConfig.CHANNEL);
-			return;
-		}
-		TextChannel textChannel = optionalChannel.get();
-		say(textChannel, text);
 	}
 
 	public static void say(TextChannel textChannel, String text) {
